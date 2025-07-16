f62578e6ee8d93beeba3d6db57c8ede9126d5f1f
Yahaya Joseph <85197681+mein22@users.noreply.github.com>
2025-07-16 01:59:32 +0100

    Update joseph.js

--- joseph.js
+++ joseph.js
@@ -1,1 +1,9 @@
+ // Get the current year
+  let currentYear = new Date().getFullYear();
+
+  // Select the paragraph element in the bottom container
+  let copyrightText = document.querySelector('.bottom-container p');
+
+  // Update the copyright text with the current year
+  copyrightText.innerHTML = `&copy; ${currentYear} Yahaya Joseph (Mein).`;
 
